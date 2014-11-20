c83a0ee413ff525d208bd5b5805c3e32073d4661
Ömer Aslan <omer.aslan@ozu.edu.tr>
2014-11-20 19:44:25 +0200

    Adding xml2json class file

---
+++ class.xml2json.php
@@ -0,0 +1,16 @@
+<?php
+
+class XML2JSON{
+	/*
+	*	Author: @OEASLAN
+	*	Website: www.oeaslan.com
+	*	Created _at_ 20/11/2014
+	*	Using: SimpleXML
+	*/
+	
+	//Starting construction
+	public function __construct(){
+
+	}
+
+}
